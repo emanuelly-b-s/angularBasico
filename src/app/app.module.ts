9928d3bf0b5ee7908af28760315bb6bd84498021
Manu <emanuellybs14@gmail.com>
2023-06-05 11:02:24 -0300

    aprendendo angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { SecondComponent } from './second/second.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SecondComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
-export class MainComponent {
 
+export class MainComponent {
+  text = 'Altere aqui..';
+  savedText = '';
+
+  onClick() {
+    this.savedText = this.text;
+  }
+  
+  update(event: any) {
+    this.text = event.target.value;
+  }
 }
