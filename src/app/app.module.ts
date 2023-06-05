6d7ce52223b5a36cb7bde86cebf4250946ecec67
Manu <emanuellybs14@gmail.com>
2023-06-05 11:43:21 -0300

    \

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { SecondComponent } from './second/second.component';
 import { NavComponent } from './nav/nav.component';
+import { ComunityPageComponent } from './comunity-page/comunity-page.component';
+import { FeedPageComponent } from './feed-page/feed-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NewAccountPageComponent } from './new-account-page/new-account-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { RecoverPageComponent } from './recover-page/recover-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    SecondComponent,
-    NavComponent
+    NavComponent,
+    ComunityPageComponent,
+    FeedPageComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    NewAccountPageComponent,
+    NotFoundPageComponent,
+    RecoverPageComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/user-page/user-page.component.spec.ts
+++ src/app/user-page/user-page.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SecondComponent } from './second.component';
+import { UserPageComponent } from './user-page.component';
 
-describe('SecondComponent', () => {
-  let component: SecondComponent;
-  let fixture: ComponentFixture<SecondComponent>;
+describe('UserPageComponent', () => {
+  let component: UserPageComponent;
+  let fixture: ComponentFixture<UserPageComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SecondComponent]
+      declarations: [UserPageComponent]
     });
-    fixture = TestBed.createComponent(SecondComponent);
+    fixture = TestBed.createComponent(UserPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
