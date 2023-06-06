5fb621c881696f091e2c7f185131938b5107e61a
Manu <emanuellybs14@gmail.com>
2023-06-06 08:48:08 -0300

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
 import { NavComponent } from './nav/nav.component';
-import { ComunityPageComponent } from './comunity-page/comunity-page.component';
-import { FeedPageComponent } from './feed-page/feed-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { NewAccountPageComponent } from './new-account-page/new-account-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { FeedPageComponent } from './feed-page/feed-page.component';
+import { ComunityPageComponent } from './comunity-page/comunity-page.component';
+import { NewAccountPageComponent } from './new-account-page/new-account-page.component';
 import { RecoverPageComponent } from './recover-page/recover-page.component';
 import { UserPageComponent } from './user-page/user-page.component';
+import { PasswordComponent } from './password/password.component';
+import { FormsModule } from '@angular/forms';
+import { CreatePasswordComponent } from './create-password/create-password.component'; // Adicionado para poder usar o ngModel
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
     NavComponent,
-    ComunityPageComponent,
-    FeedPageComponent,
-    HomePageComponent,
     LoginPageComponent,
-    NewAccountPageComponent,
+    HomePageComponent,
     NotFoundPageComponent,
+    FeedPageComponent,
+    ComunityPageComponent,
+    NewAccountPageComponent,
     RecoverPageComponent,
-    UserPageComponent
+    UserPageComponent,
+    PasswordComponent,
+    CreatePasswordComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // Adicionado para poder usar o ngModel
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+
+export class AppModule {}
